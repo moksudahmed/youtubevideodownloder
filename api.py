45df9d12ad9019bbdd1c8605d2263ac930b5d657
Khondakar M Ahmed <k.moksud@gmail.com>
2023-01-21 19:58:27 +0600

    api file added

---
+++ api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, Response
+import youtube_dl
+
+app = Flask(__name__)
+
+@app.route('/download', methods=['GET'])
+def download_video():
+    url = request.args.get('url')
+    ydl_opts = {
+        'outtmpl': '%(title)s.%(ext)s',
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    return Response(ydl.prepare_filename(ydl.extract_info(url)),
+                    mimetype="audio/mpeg",
+                    headers={"Content-disposition": "attachment; filename={}.mp3".format(ydl.extract_info(url)["title"])})
+
+if __name__ == '__main__':
+    app.run()
