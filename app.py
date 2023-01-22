e18598e7f059b703dbab4a7783f4b01ea9aff77e
moksudahmed <k.moksud@gmail.com>
2023-01-22 12:02:13 +0600

    qr added

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, request, Response
 import youtube_dl
+import qrcode
+from io import BytesIO
+from flask import Flask, request, send_file
+import os
+import PIL
 
 app = Flask(__name__)
 @app.route('/')
@@ -11,18 +16,20 @@ def download_video():
     url = request.args.get('url')
     ydl_opts = {
         'outtmpl': '%(title)s.%(ext)s',
-        'format': 'bestaudio/best',
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',
-            'preferredcodec': 'mp3',
-            'preferredquality': '192',
-        }],
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
-    return Response(ydl.prepare_filename(ydl.extract_info(url)),
-                    mimetype="audio/mpeg",
-                    headers={"Content-disposition": "attachment; filename={}.mp3".format(ydl.extract_info(url)["title"])})
+    filename = ydl.prepare_filename(ydl.extract_info(url))
+    return send_file(filename, as_attachment=True, attachment_filename='{}.mp4'.format(ydl.extract_info(url)["title"]))
+
+@app.route("/qr")
+def qr():
+    url = request.args.get('url')
+    img = qrcode.make(url)
+    img_io = BytesIO()
+    img.save(img_io, 'PNG')
+    img_io.seek(0)
+    return Response(img_io.getvalue(), mimetype='image/png', headers={'Content-Disposition':'attachment;filename=qrcode.png'})
 
 if __name__ == '__main__':
-     app.run(debug=True)
+    app.run(debug=True)
